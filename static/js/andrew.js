26ebc59a6b4297153b5a18bafc0e972104a01c30
andrewzee23 <andrewazamora@gmail.com>
2020-12-16 11:19:37 -0600

    minor changes

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -1,11 +1,9 @@
 const decline_url = '/api/v1.0/decline'
 
-fetch(decline_url)
+getData().catch(err => console.log(err));
 
-    .then(response => response.json())
-    .then((data) => {
-      let allData = JSON.parse(data);
-
-      console.log(allData)
-})
-.catch(err => console.log(err));
+async function getData() {
+  const response = await fetch(decline_url);
+  const data = await response.json();
+  console.log(data);
+}
