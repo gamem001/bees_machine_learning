961cb986e8eb8205d77ac1bfe78f272825ce55f0
andrewzee23 <andrewazamora@gmail.com>
2020-12-16 11:45:43 -0600

    syntax order fix

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -1,9 +1,9 @@
 const declineUrl = '/api/v1.0/decline'
 
-getData().catch(err => console.log(err));
-
 async function getData() {
   const response = await fetch(declineUrl);
   const data = await response.json();
   console.log(data);
 }
+
+getData().catch(err => console.log(err));
