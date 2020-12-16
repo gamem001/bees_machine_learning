70a88e93c53ec691777386e3aa92d5b91242f7a6
andrewzee23 <andrewazamora@gmail.com>
2020-12-16 12:12:08 -0600

    parsing json data

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -3,7 +3,10 @@ const declineUrl = '/api/v1.0/decline'
 async function getData() {
   const response = await fetch(declineUrl);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
+  data.forEach(event => {
+    const state = event.state
+    const year = event.year  
+  });
 }
-
 getData().catch(err => console.log(err));
