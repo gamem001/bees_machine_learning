1869c9bcfb83d2c18c0e75c98f5a6db101ee112d
andrewzee23 <andrewazamora@gmail.com>
2020-12-16 11:42:49 -0600

    fixed syntax

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -1,9 +1,9 @@
-const decline_url = '/api/v1.0/decline'
+const declineUrl = '/api/v1.0/decline'
 
 getData().catch(err => console.log(err));
 
 async function getData() {
-  const response = await fetch(decline_url);
+  const response = await fetch(declineUrl);
   const data = await response.json();
   console.log(data);
 }
