b7e47ee402ae78888ff88b26e304a0223979f572
andrewzee23 <andrewazamora@gmail.com>
2020-12-21 11:01:49 -0600

    plot sized better

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -1,8 +1,8 @@
 //
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 760 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
