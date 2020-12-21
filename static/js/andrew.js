f1d5603a40f4cc9d6fd401b4fc22b88cbe2c6dbb
andrewzee23 <andrewazamora@gmail.com>
2020-12-21 11:04:10 -0600

    removed comments

--- static/js/andrew.js
+++ static/js/andrew.js
@@ -1,10 +1,9 @@
-//
-// set the dimensions and margins of the graph
+
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 760 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+
 var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -13,27 +12,27 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialize the X axis
+
 var x = d3.scaleBand()
   .range([ 0, width ])
   .padding(1);
 var xAxis = svg.append("g")
   .attr("transform", "translate(0," + height + ")")
 
-// Initialize the Y axis
+
 var y = d3.scaleLinear()
   .range([ height, 0]);
 var yAxis = svg.append("g")
   .attr("class", "myYaxis")
 
 
-// A function that create update the plot for a given variable:
+
 function update(selectedVar) {
 
-  // Parse the Data
+  
   d3.json("/api/v1.0/decline", function(data) {
 
-    // X axis
+    
     x.domain(data.map(function(d) { return d.state; }))
     xAxis
       .transition()
@@ -47,14 +46,14 @@ function update(selectedVar) {
       .attr('transform','rotate(90)')
       .style('text-anchor','start')
 
-    // Add Y axis
+    
     y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
     yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
-    // variable u: map data to existing circle
+    
     var j = svg.selectAll(".myLine")
       .data(data)
-    // update lines
+    
     j
       .enter()
       .append("line")
@@ -69,10 +68,10 @@ function update(selectedVar) {
         .attr("stroke", "orange")
 
 
-    // variable u: map data to existing circle
+    
     var u = svg.selectAll("circle")
       .data(data)
-    // update bars
+    
     u
       .enter()
       .append("circle")
@@ -89,5 +88,5 @@ function update(selectedVar) {
 
 }
 
-// Initialize plot
+
 update('deadout')
